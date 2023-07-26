208f286a8d1e76bac4be155a09bbb85dcbc9aeff
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2023-07-26 15:34:37 +0100

    feat(web): add an error page and handle 404s

--- web/src/app/errors.js
+++ web/src/app/errors.js
@@ -6,5 +6,37 @@ import logger from '../util/logger.js';
  * @type {import('express').ErrorRequestHandler}
  */
 export function defaultErrorHandler(error, req, res, next) {
-    logger.error(error.message || 'unknown error', error);
+    const message = error.message || 'unknown error'
+    logger.error(message, error);
+
+    if (res.headersSent) {
+        next(error);
+        return;
+    }
+
+    const code = error.statusCode || 500;
+    res.status(code);
+    res.render(`layouts/error`, { 
+        pageTitle: 'Sorry, there was an error',
+        messages: [
+            message,
+            'Try again later'
+        ]
+     });
+}
+
+/**
+ * A catch-all handler to serve a 404 page
+ *
+ * @type {import('express').RequestHandler}
+ */
+export function notFoundHandler(req, res) {
+    res.status(404);
+    res.render(`layouts/error`, {
+        pageTitle: 'Page not found',
+        messages: [
+            'If you typed the web address, check it is correct.',
+            'If you pasted the web address, check you copied the entire address.'
+        ]
+    });
 }

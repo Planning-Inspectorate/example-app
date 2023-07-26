08c732a0f65a604b1acabd8d634aab4c014d3c02
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2023-07-26 15:13:22 +0100

    chore(web): add src and static directories to config

--- web/src/app/config.js
+++ web/src/app/config.js
@@ -1,7 +1,16 @@
 import dotenv from 'dotenv';
+import path from 'node:path';
+import {fileURLToPath} from 'url';
 
 // load configuration from .env file into process.env
 dotenv.config();
+
+// get the file path for the directory this file is in
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+// get the file path for the web/src directory
+const srcDir = path.join(dirname, '..');
+// get the file path for the .static directory
+const staticDir = path.join(srcDir, '.static');
 
 // export configuration, with defaults if they are not set
 export default {
@@ -10,5 +19,9 @@ export default {
     // the log level to use
     logLevel: process.env.LOG_LEVEL || 'info',
     // the HTTP port to listen on
-    httpPort: process.env.HTTP_PORT || 8080
+    httpPort: process.env.HTTP_PORT || 8080,
+    // the web/src directory
+    srcDir,
+    // the static directory to serve assets from (images, css, etc..)
+    staticDir
 };

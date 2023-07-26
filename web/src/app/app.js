f65fdc92e9b62fce1d86f4c0885d48a4de2cc232
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2023-07-26 15:15:14 +0100

    feat(web): add nunjucks as the view engine

--- web/src/app/app.js
+++ web/src/app/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { defaultErrorHandler } from './errors.js';
 import router from './router.js';
+import nunjucksEnvironment from './nunjucks.js';
 
 // create an express app, and configure it for our usage
 const app = express();
@@ -10,6 +11,11 @@ const app = express();
 // see https://expressjs.com/en/resources/middleware/body-parser.html
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Set the express view engine to nunjucks
+// calls to res.render will use nunjucks
+nunjucksEnvironment.express(app);
+app.set('view engine', 'njk');
 
 // register the router, which will define any subpaths
 // any paths not defined will return 404 by default
